048a3b24edb700c84b1dab6f8ef19f9e8112153c
Rafaely Zanão <rafaelyobzutzanao@gmail.com>
2023-11-15 20:07:52 -0300

    comentarios finais

--- script.js
+++ script.js
@@ -1,64 +1,66 @@
-let count = 1
+let count = 1;
 document.getElementById("radio1").checked = true;
 
 setInterval(function () {
-    nextImage();
+  nextImage();
 }, 8000);
 
 function nextImage() {
-    count++;
-    if (count > 4) {
-        count = 1;
-    }
+  count++;
+  if (count > 4) {
+    count = 1;
+  }
 
-    document.getElementById("radio" + count).checked = true;
+  document.getElementById("radio" + count).checked = true;
 }
-const guias = document.querySelectorAll('.tab');
-const conteudoFilmes = document.querySelectorAll('.filmes');
+
+const guias = document.querySelectorAll(".tab");
+const conteudoFilmes = document.querySelectorAll(".filmes");
 
 guias.forEach((guia, index) => {
-    guia.addEventListener('click', () => {
-        guias.forEach((tab) => tab.classList.remove('active'));
-        guia.classList.add('active');
+  guia.addEventListener("click", () => {
+    guias.forEach((tab) => tab.classList.remove("active"));
+    guia.classList.add("active");
 
-        conteudoFilmes.forEach((conteudo, i) => {
-            if (index === i) {
-                conteudo.classList.add('active');
-            } else {
-                conteudo.classList.remove('active');
-            }
-        });
+    conteudoFilmes.forEach((conteudo, i) => {
+      if (index === i) {
+        conteudo.classList.add("active");
+      } else {
+        conteudo.classList.remove("active");
+      }
     });
+  });
 });
 
-
 class DecadeFilter {
-    constructor() {
-        this.init();
-    }
+  constructor() {
+    this.init();
+  }
 
-    init() {
-        const decadeRadios = document.querySelectorAll('input[name="decade-filter"]');
-        const filmeElements = document.querySelectorAll('.filme');
+  init() {
+    const decadeRadios = document.querySelectorAll(
+      'input[name="decade-filter"]'
+    );
+    const filmeElements = document.querySelectorAll(".filme");
 
-        decadeRadios.forEach(radio => {
-            radio.addEventListener('change', () => {
-                this.filterByDecade(radio.value, filmeElements);
-            });
-        });
-    }
+    decadeRadios.forEach((radio) => {
+      radio.addEventListener("change", () => {
+        this.filterByDecade(radio.value, filmeElements);
+      });
+    });
+  }
 
-    filterByDecade(selectedDecade, filmeElements) {
-        filmeElements.forEach(filme => {
-            const filmeDecade = filme.getAttribute('data-decade');
+  filterByDecade(selectedDecade, filmeElements) {
+    filmeElements.forEach((filme) => {
+      const filmeDecade = filme.getAttribute("data-decade");
 
-            if (selectedDecade === 'all' || filmeDecade === selectedDecade) {
-                filme.style.display = 'block';
-            } else {
-                filme.style.display = 'none';
-            }
-        });
-    }
+      if (selectedDecade === "all" || filmeDecade === selectedDecade) {
+        filme.style.display = "block";
+      } else {
+        filme.style.display = "none";
+      }
+    });
+  }
 }
 
 const decadeFilter = new DecadeFilter();
@@ -66,23 +68,24 @@ const decadeFilter = new DecadeFilter();
 let currentIndex = 0;
 
 function showSlide(index) {
-    const slides = document.querySelectorAll('.movie-slide');
-    if (index < 0) {
-        currentIndex = slides.length - 1;
-    } else if (index >= slides.length) {
-        currentIndex = 0;
-    } else {
-        currentIndex = index;
-    }
+  const slides = document.querySelectorAll(".movie-slide");
+  if (index < 0) {
+    currentIndex = slides.length - 1;
+  } else if (index >= slides.length) {
+    currentIndex = 0;
+  } else {
+    currentIndex = index;
+  }
 
-    const offset = -currentIndex * 100 + '%';
-    document.querySelector('.carousel-container').style.transform = 'translateX(' + offset + ')';
+  const offset = -currentIndex * 100 + "%";
+  document.querySelector(".carousel-container").style.transform =
+    "translateX(" + offset + ")";
 }
 
 function prevSlide() {
-    showSlide(currentIndex - 1);
+  showSlide(currentIndex - 1);
 }
 
 function nextSlide() {
-    showSlide(currentIndex + 1);
+  showSlide(currentIndex + 1);
 }
